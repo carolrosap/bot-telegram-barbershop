302e44a37453e8cc9fe1b6f3a575e9d644467c39
Caroline Rosa <cpauladarosa@gmail.com>
2023-06-30 01:47:27 -0300

    agendamento e consulta agendamentos funcionando

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const axios = require("axios");
 let chatStates = {};
 let servicos = [];
 let professionals = [];
-let schedules = []
-let sendObjectAPI  = {
+let schedules = [];
+let sendObjectAPI = {
   schedule_id: null,
-  service_id: null, 
+  service_id: null,
   user_id: null
 }
 
@@ -20,14 +20,16 @@ const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 const stateOptions = {
   1: ["Realizar reserva", "Consultar reserva", "Cancelar reserva"],
   2: [],  // Populado dinamicamente quando o usuário escolhe "Realizar reserva".
-  3: []
+  3: [],
+  4: []
 };
 
 // Define as funções de manipulação para cada estado.
 const stateHandlers = {
   1: handleState1,
   2: handleState2,
-  3: handleState3
+  3: handleState3,
+  4: handleState4
 };
 
 const validaUser = async (chatId) => {
@@ -109,6 +111,9 @@ bot.on("message", async (msg) => {
   if (!msg.contact && msg.text !== "/start") {
     const chatId = msg.chat.id;
     const userState = chatStates[chatId];
+    if (datas[chatId] === undefined) {
+      datas[chatId] = { sendObjectAPI, professional_id: null, user_name: null };
+    }
 
     // Se o estado atual do usuário não tem um manipulador definido, registre o usuário.
     if (!stateHandlers[userState]) {
@@ -117,8 +122,9 @@ bot.on("message", async (msg) => {
         saudacao(chatId, valida.data.name);
         chatStates[chatId] = 1;
         const user_id = valida.data.id;
+        const user_name = valida.data.nome;
+        datas[chatId].user_name = user_name;
         datas[chatId].sendObjectAPI.user_id = user_id;
-        console.log(datas[chatId]);
       } else {
         return registerUser(msg, chatId);
       }
@@ -141,18 +147,18 @@ bot.on("contact", async (msg) => {
 
 bot.onText(/\/start/, async (msg) => {
   const chatId = msg.chat.id;
-  if(datas[chatId] === undefined) {
-    datas[chatId] = {sendObjectAPI, professional_id: null};
+  if (datas[chatId] === undefined) {
+    datas[chatId] = { sendObjectAPI, professional_id: null, user_name: null };
   }
-
   try {
     const response = await validaUser(chatId);
     if (response.exists) {
       saudacao(chatId, response.data.name);
       chatStates[chatId] = 1;
       const user_id = response.data.id;
+      const user_name = response.data.name;
+      datas[chatId].user_name = user_name;
       datas[chatId].sendObjectAPI.user_id = user_id;
-      console.log(datas[chatId]);
     } else {
       bot.sendMessage(
         chatId,
@@ -178,7 +184,6 @@ bot.onText(/\/start/, async (msg) => {
 });
 
 async function handleState1(msg, chatId) {
-  //console.log("", msg.text)
   switch (msg.text) {
     case "Realizar reserva":
       const options = [];
@@ -190,15 +195,28 @@ async function handleState1(msg, chatId) {
             options.push([each.category.name]);
           });
         }
-        stateOptions[2] = options;  // Atualiza as opções para o estado 2.
-        chatStates[chatId] = 2;  // Muda o estado para 2.
+        stateOptions[2] = options;
+        chatStates[chatId] = 2;
         mostrarOpcoes(chatId, "Selecione o serviço desejado:", options);
       } catch (error) {
         errorMsg(chatId, error);
       }
       break;
     case "Consultar reserva":
-      bot.sendMessage(chatId, "Você escolheu Consultar reserva. Vamos processar isso.");
+      //bot.sendMessage(chatId, "Você escolheu Consultar reserva. Vamos processar isso.");
+      const resposta = await consultaReservas(chatId);
+      if(resposta.exists) {
+        let msg = 'Você tem os seguintes horários agendados: \n'
+        resposta.data.map((sch => {
+          msg = msg + 'Dia ' + formatDate(sch.schedule.date) + ' às ' + formatTime(sch.schedule.time) + ' ' + sch.service.category.name + ' com o barbeiro ' + sch.service.professional.name + ' \n'
+        }));
+        msg = msg + '\nVolte ao menu para mais opções.';
+        bot.sendMessage(chatId, msg);
+      }else{
+        bot.sendMessage(chatId, "Você não tem nenhum horário agendado ainda, faça um agendamento!");
+      }
+      chatStates[chatId] = 1;
+      saudacao(chatId, datas[chatId].user_name);  
       break;
     case "Cancelar reserva":
       bot.sendMessage(chatId, "Você escolheu Cancelar reserva. Vamos processar isso.");
@@ -207,7 +225,6 @@ async function handleState1(msg, chatId) {
 }
 
 async function handleState2(msg, chatId) {
-  //sendObjectAPI.nivel2 = msg.text;
   const service = servicos.find((item) => item.category.name === msg.text);
   let options = [];
   if (service) {
@@ -226,37 +243,106 @@ async function handleState2(msg, chatId) {
     professionals.map((prof) => {
       options.push([prof.name])
     });
-    stateOptions[3] = options;  // Atualiza as opções para o estado 3.
-    chatStates[chatId] = 3;  // Muda o estado para 3.
+    stateOptions[3] = options;
+    chatStates[chatId] = 3;
     mostrarOpcoes(chatId, "Certo! Com qual barbeiro gostaria de realizar o serviço?", options);
-
   }
-
 }
 
 async function handleState3(msg, chatId) {
-  console.log("terceiro", datas[chatId]);
-  const professional = professionals.find((prof) => prof.name === msg.text);  
+  const professional = professionals.find((prof) => prof.name === msg.text);
   let options = [];
   if (professional) {
     const professionalId = professional.id;
-    datas[chatId].professional_id =  professionalId; 
+    datas[chatId].professional_id = professionalId;
+    let schedulesAux = []
     try {
       const response = await axios.get(
         `${process.env.API_URL}/schedule?available=1&userId=${professionalId}`
       );
       if (response.status === 200) {
-        schedules = response.data.length > 0 ? response.data : {};
+        schedulesAux = response.data.length > 0 ? response.data : {};
       }
     } catch (error) {
       errorMsg(chatId, error);
     }
-    schedules.map((sch) => {
-      const exibicao = sch.date + " às " + sch.time
-      options.push([exibicao])
+    schedulesAux.map((sch) => {
+      const exibicao = formatDate(sch.date) + " às " + formatTime(sch.time);
+      options.push([exibicao]);
+      schedules.push({ ...sch, exhibition: exibicao });
     });
-    stateOptions[4] = options;  // Atualiza as opções para o estado 3.
-    chatStates[chatId] = 4;  // Muda o estado para 3.
+    stateOptions[4] = options;
+    chatStates[chatId] = 4;
     mostrarOpcoes(chatId, "Escolha um dos horários disponíveis: ", options);
   }
 }
+
+async function handleState4(msg, chatId) {
+  const schedule = schedules.find((sch) => sch.exhibition === msg.text);
+  if (schedule) {
+    const scheduleId = schedule.id;
+    datas[chatId].sendObjectAPI.schedule_id = scheduleId;
+  }
+  await saveData(chatId);
+}
+
+async function saveData(chatId) {
+  try {
+    const response = await axios.post(`${process.env.API_URL}/scheduling`, datas[chatId].sendObjectAPI);
+    if (response.status === 201) {
+      bot.sendMessage(
+        chatId,
+        "Horário reservado com sucesso!\nAgora você pode voltar ao menu para selecionar outras opções!"
+      );
+      chatStates[chatId] = 1;
+      datas[chatId].sendObjectAPI.schedule_id = null;
+      datas[chatId].sendObjectAPI.service_id = null;
+      datas[chatId].professional_id = null;
+    }
+  } catch (error) {
+    console.error(error);
+    bot.sendMessage(
+      chatId,
+      "Houve um erro ao fazer a reserva. Por favor, tente novamente mais tarde."
+    );
+  }
+  saudacao(chatId, datas[chatId]?.user_name);
+
+}
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const day = ("0" + date.getDate()).slice(-2);
+  const month = ("0" + (date.getMonth() + 1)).slice(-2);
+  const year = date.getFullYear();
+  const formattedDate = `${day}/${month}/${year}`;
+  return formattedDate;
+}
+
+const formatTime = (timeString) => {
+  const parts = timeString.split(":");
+  const formattedTime = `${parts[0]}:${parts[1]}`;
+  return formattedTime;
+}
+
+const consultaReservas = async (chatId) => {
+  const resposta = {
+    exists: false,
+    data: {},
+  };
+  const user_id = datas[chatId].sendObjectAPI.user_id;
+  try {
+    const response = await axios.get(
+      `${process.env.API_URL}/scheduling?userId=${user_id}`
+    );
+    if (response.status === 200) {
+      if (response.data.length > 0 ) {
+        resposta.exists = true;
+        resposta.data = response.data;
+      }
+    }
+    return resposta;
+  } catch (error) {
+    errorMsg(chatId, error);
+  }
+};
